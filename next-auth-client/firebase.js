0b702f53cc7773d14081edb96d5ca27fe2dd8014
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-09-16 12:51:15 +1000

    Configured firebase

---
+++ next-auth-client/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCsOJUK2X8_GFt889m-lTqT3_8GD8wl0RE",
+  authDomain: "auth-development-8e630.firebaseapp.com",
+  projectId: "auth-development-8e630",
+  storageBucket: "auth-development-8e630.appspot.com",
+  messagingSenderId: "261788341436",
+  appId: "1:261788341436:web:3db6cb04d9bdbb1bdb2cb9",
+  measurementId: "G-34TYW9XTR8"
+};
+
+// Initialize Firebase
+if(firebaseConfig.apps.length) {
+    const app = initializeApp(firebaseConfig);
+}
+
+export default firebase;
--- next-auth-client/pages/_app.js
+++ next-auth-client/pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import "bootstrap/dist/css/bootstrap.css"
+import Nav from "../components/Nav"
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Nav/>
+    <Component {...pageProps} />
+  </>
 }
+
+
 
 export default MyApp
