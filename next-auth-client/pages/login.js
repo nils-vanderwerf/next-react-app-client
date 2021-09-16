730e5be74c7eb2c0c85cc58b037ca5174465da3b
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-09-16 13:48:46 +1000

    Built login page

--- next-auth-client/pages/login.js
+++ next-auth-client/pages/login.js
@@ -1,9 +1,34 @@
+import { useRouter } from 'next/dist/client/router'
 import React from 'react'
+import { useState } from 'react'
 
 export default function Login() {
+    const [email, setEmail] = useState("")
+    const [pass, setPass] = useState("")
+    const router = useRouter()
     return (
         <div>
-            <h2>Login Page</h2>
+            <h2 className="text-center pt-4">Login Page</h2>
+            <div className="col-md-8">
+                <div className="form-group">
+                    <label>Email</label>
+                    <input type="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                    className="form-control"/>
+                    <small>We'll never share your email</small>
+                </div>
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="email"
+                    onChange={(e) => setPass(e.target.value)}
+                    value={pass}
+                    className="form-control"/>
+                </div>
+                <button className="btn btn-primary w-100">
+                    Login
+                </button>
+            </div>       
         </div>
     )
 }
